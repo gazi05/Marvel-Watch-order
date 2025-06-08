26f135a5f8701be3a11cd195622ccd1a92f9fe9b
Ghazi <gazi.alqmmaz12@gmail.com>
2025-06-08 22:02:53 +0300

    added the watch order page

---
+++ movies.js
@@ -0,0 +1,44 @@
+movies = [
+  {
+    title : "Iron man", 
+    year : 2008,
+    poster : 'images/iron-man.jpg',
+    phase : "Phase 1",
+    order : 1
+  },
+  {
+    title : "The Incredible Hulk",
+    year : 2008,
+    poster : 'images/The-Incredible-Hulk.jpg',
+    phase : "Phase 1",
+    order : 2
+  },
+  {
+    title : "Iron Man 2",
+    year : 2010,
+    poster : 'images/rIon-man-2.jpeg',
+    phase : "Phase 1",
+    order : 3
+  },
+  {
+    title : "Thor",
+    year : 2011,
+    poster : 'images/thor.jpeg',
+    phase : "Phase 1",
+    order : 4
+  },
+  {
+    title : "Captain America: The First Avenger",
+    year : 2011,
+    poster : 'images/Captain-America-The-First-Avenger.jpeg',
+    phase : "Phase 1",
+    order : 5
+  },
+  {
+    title : "The Avengers",
+    year : 2012,
+    poster : 'images/avengers.jpeg',
+    phase : "Phase 1",
+    order : 6
+  }
+]
