943f13fb74d1c042939a06d10e737bbce18d8fc6
Ghazi <gazi.alqmmaz12@gmail.com>
2025-06-09 10:18:22 +0300

    added the cart movies

--- movies.js
+++ movies.js
@@ -2,7 +2,7 @@ const movies = [
   {
     title : "Iron man", 
     year : 2008,
-    poster : 'images/iron-man.jpg',
+    poster : './images/iron-man.jpg',
     phase : "Phase 1",
     order : 1 , 
     description : "Genius billionaire Tony Stark builds a powered armor suit to escape captivity and becomes the armored superhero Iron Man."
@@ -10,7 +10,7 @@ const movies = [
   {
     title : "The Incredible Hulk",
     year : 2008,
-    poster : 'images/The-Incredible-Hulk.jpg',
+    poster : './images/The-Incredible-Hulk.jpg',
     phase : "Phase 1",
     order : 2 , 
     description : "Scientist Bruce Banner seeks a cure for his gamma radiation-induced condition, transforming him into the monstrous Hulk when angered."
@@ -18,7 +18,7 @@ const movies = [
   {
     title : "Iron Man 2",
     year : 2010,
-    poster : 'images/Iron-Man-2.jpeg',
+    poster : './images/Iron-Man-2.jpeg',
     phase : "Phase 1",
     order : 3 ,
     description : "Tony Stark faces new enemies and struggles with his declining health while revealing his identity as Iron Man to the world."
@@ -26,7 +26,7 @@ const movies = [
   {
     title : "Thor",
     year : 2011,
-    poster : 'images/thor.jpeg',
+    poster : './images/thor.jpeg',
     phase : "Phase 1",
     order : 4 , 
     description : "The powerful but arrogant Norse god Thor is banished to Earth and learns humility while protecting humanity from dark forces."
@@ -34,7 +34,7 @@ const movies = [
   {
     title : "Captain America: The First Avenger",
     year : 2011,
-    poster : 'images/Captain-America-The-First-Avenger.jpeg',
+    poster : './images/Captain-America-The-First-Avenger.jpeg',
     phase : "Phase 1",
     order : 5 ,
     description : "During World War II, Steve Rogers is transformed into super-soldier Captain America and battles the villainous Red Skull."
@@ -42,9 +42,9 @@ const movies = [
   {
     title : "The Avengers",
     year : 2012,
-    poster : 'images/The-Avengers.jpeg',
+    poster : './images/The-Avengers.jpeg',
     phase : "Phase 1",
-    order : 6 , 
+    order : 6 ,
     description : "Earth's mightiest heroes — Iron Man, Captain America, Thor, Hulk, Black Widow, and Hawkeye — unite to stop Loki and an alien invasion."
   }
 ]
