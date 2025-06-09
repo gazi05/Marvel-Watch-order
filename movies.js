4b68a66c3eafed7847ac076af831022dc454a867
Ghazi <gazi.alqmmaz12@gmail.com>
2025-06-09 13:02:55 +0300

    added the release and timeline option

--- movies.js
+++ movies.js
@@ -5,7 +5,9 @@ const movies = [
     poster : './images/iron-man.jpg',
     phase : "Phase 1",
     order : 1 , 
-    description : "Genius billionaire Tony Stark builds a powered armor suit to escape captivity and becomes the armored superhero Iron Man."
+    timeline : 3 ,
+    description : "Genius billionaire Tony Stark builds a powered armor suit to escape captivity and becomes the armored superhero Iron Man." ,
+    trailer : "https://www.youtube.com/watch?v=8ugaeA-nMTc"
   },
   {
     title : "The Incredible Hulk",
@@ -13,7 +15,9 @@ const movies = [
     poster : './images/The-Incredible-Hulk.jpg',
     phase : "Phase 1",
     order : 2 , 
-    description : "Scientist Bruce Banner seeks a cure for his gamma radiation-induced condition, transforming him into the monstrous Hulk when angered."
+    timeline : 5 ,
+    description : "Scientist Bruce Banner seeks a cure for his gamma radiation-induced condition, transforming him into the monstrous Hulk when angered.",
+    trailer : "https://www.youtube.com/watch?v=xbqNb2PFKKA"
   },
   {
     title : "Iron Man 2",
@@ -21,7 +25,9 @@ const movies = [
     poster : './images/Iron-Man-2.jpeg',
     phase : "Phase 1",
     order : 3 ,
-    description : "Tony Stark faces new enemies and struggles with his declining health while revealing his identity as Iron Man to the world."
+    timeline : 4 ,
+    description : "Tony Stark faces new enemies and struggles with his declining health while revealing his identity as Iron Man to the world.",
+    trailer : "https://www.youtube.com/watch?v=BoohRoVA9WQ"
   },
   {
     title : "Thor",
@@ -29,7 +35,9 @@ const movies = [
     poster : './images/Thor.jpeg',
     phase : "Phase 1",
     order : 4 , 
-    description : "The powerful but arrogant Norse god Thor is banished to Earth and learns humility while protecting humanity from dark forces."
+    timeline : 6 , 
+    description : "The powerful but arrogant Norse god Thor is banished to Earth and learns humility while protecting humanity from dark forces.",
+    trailer : "https://www.youtube.com/watch?v=JOddp-nlNvQ"
   },
   {
     title : "Captain America: The First Avenger",
@@ -37,7 +45,9 @@ const movies = [
     poster : './images/Captain-America-The-First-Avenger.jpeg',
     phase : "Phase 1",
     order : 5 ,
-    description : "During World War II, Steve Rogers is transformed into super-soldier Captain America and battles the villainous Red Skull."
+    timeline : 1 ,
+    description : "During World War II, Steve Rogers is transformed into super-soldier Captain America and battles the villainous Red Skull.",
+    trailer : "https://www.youtube.com/watch?v=JerVrbLldXw"
   },
   {
     title : "The Avengers",
@@ -45,7 +55,69 @@ const movies = [
     poster : './images/The-Avengers.jpeg',
     phase : "Phase 1",
     order : 6 ,
-    description : "Earth's mightiest heroes — Iron Man, Captain America, Thor, Hulk, Black Widow, and Hawkeye — unite to stop Loki and an alien invasion."
+    timeline : 7  ,
+    description : "Earth's mightiest heroes — Iron Man, Captain America, Thor, Hulk, Black Widow, and Hawkeye — unite to stop Loki and an alien invasion.",
+    trailer : "https://www.youtube.com/watch?v=eOrNdBpGMv8"
+  },
+  {
+    title : "Iron man 3",
+    year : 2013,
+    poster : './images/Iron-Man-3.jpg',
+    phase : "Phase 2",
+    order : 7 ,
+    timeline : 8 ,  
+    description : "Tony Stark faces a formidable enemy known as the Mandarin while dealing with the aftermath of the Battle of New York.",
+    trailer : "https://www.youtube.com/watch?v=Ke1Y3P9D0Bc"
+  },
+  {
+    title : "Thor: The Dark World",
+    year : 2013,
+    poster : './images/Thor-The-Dark-World.jpeg',
+    phase : "Phase 2",
+    order : 8 ,
+    timeline : 9 ,
+    description : "Thor battles dark elves who seek to control the Aether, a powerful reality-altering substance.",
+    trailer : "https://www.youtube.com/watch?v=npvJ9FTgZbM"
+  },
+  {
+    title : "Captain America: The Winter Soldier",
+    year : 2014,
+    poster : './images/Captain-America-The-Winter-Soldier.jpeg',
+    phase : "Phase 2",
+    order : 9 ,
+    timeline : 10 ,
+    description : "Captain America teams up with Black Widow and Falcon to uncover a conspiracy within S.H.I.E.L.D. and confronts the mysterious Winter Soldier.",
+    trailer : "https://www.youtube.com/watch?v=7k_6NsYIipA"
+  },
+  {
+    title : "Guardians of the Galaxy",
+    year : 2014,
+    poster : './images/Guardians-of-the-Galaxy.jpeg',
+    phase : "Phase 2",
+    order : 10 ,
+    timeline : 11 ,
+    description : "A group of intergalactic misfits, including Star-Lord, Gamora, Drax, Rocket, and Groot, band together to save the galaxy from a powerful villain.",
+    trailer : "https://www.youtube.com/watch?v=d96cjJhvlMA"
+  },
+  {
+    title : "Avengers: Age of Ultron",
+    year : 2015,
+    poster : './images/Avengers-Age-of-Ultron.jpeg',
+    phase : "Phase 2",
+    order : 11 ,
+    timeline : 13 ,
+    description : "The Avengers must stop the rogue AI Ultron from exterminating humanity while dealing with new threats like Scarlet Witch and Quicksilver.",
+    trailer : "https://www.youtube.com/watch?v=zi4VAPVYFA4"
+  },
+  {
+    title : "Ant-Man",
+    year : 2015,
+    poster : './images/Ant-man-1.jpg',
+    phase : "Phase 2",
+    order : 12 ,
+    timeline : 14 ,
+    description : "Scott Lang, a former thief, must embrace his inner hero and help his mentor, Hank Pym, protect the Ant-Man technology from a new generation of towering threats.",
+    trailer : "https://www.youtube.com/watch?v=pWdKf3MneyI"
   }
 ]
 
